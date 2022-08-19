189b319c88be91f80657697a4ae1076e6ee49917
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-19 10:52:48 +0300

    add response controller

--- src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
+++ src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
@@ -7,4 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootProjectApplication.class, args
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
+
+}
--- src/main/java/com/example/spring_boot_project/controller/HelloController.java
+++ src/main/java/com/example/spring_boot_project/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.spring_boot_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +13,13 @@ public class HelloController {
     public String hello() {
 
         return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("text1") String str, Model model) {
+        // Register the string received from the screen to Model
+        model.addAttribute("sample", str);
+// Screen transition to response.html
+        return "hello/response";
     }
 }
