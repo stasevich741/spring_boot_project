44176c4adbb09101dfbd68800b693dba6ff4a962
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-20 09:29:45 +0300

    add message properties

--- src/main/java/com/example/spring_boot_project/service/UserApplicationService.java
+++ src/main/java/com/example/spring_boot_project/service/UserApplicationService.java
@@ -1,17 +1,25 @@
 package com.example.spring_boot_project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
 public class UserApplicationService {
 
+    @Autowired
+    private MessageSource messageSource;
+
     public Map<String, Integer> getGenderMap() {
         Map<String, Integer> genderMap = new HashMap<>();
-        genderMap.put("male", 1);
-        genderMap.put("female", 2);
+        String male = messageSource.getMessage("male", null, Locale.ENGLISH);
+        String female = messageSource.getMessage("female", null, Locale.ENGLISH);
+        genderMap.put(male, 1);
+        genderMap.put(female, 2);
         return genderMap;
     }
 }
