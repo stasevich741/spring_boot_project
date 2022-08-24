d7ca5c0853b29d5938975cb0154d9eba64814608
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-24 22:37:20 +0300

    add validation annotation

--- src/main/java/com/example/spring_boot_project/form/SignupForm.java
+++ src/main/java/com/example/spring_boot_project/form/SignupForm.java
@@ -1,17 +1,35 @@
 package com.example.spring_boot_project.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
 public class SignupForm {
+
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min = 6, max = 15, message = "password length min 6 max 15")
+    @Pattern(regexp = "^[a-zA-Z\\d]+$")
     private String password;
+
+    @NotBlank
     private String userName;
+
     @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private Date birthday;
+
+    @Min(18)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 }
