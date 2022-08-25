862b9d9b3d69af0519484cc403c1d33f93aa1544
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-25 15:53:02 +0300

    add screen layouts closed #5

--- src/main/java/com/example/spring_boot_project/controller/LoginController.java
+++ src/main/java/com/example/spring_boot_project/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.spring_boot_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -9,5 +10,13 @@ public class LoginController {
     @GetMapping("/login")
     public String getLogin() {
         return "login/login";
+    }
+
+    /**
+     * Redirect to user list screen
+     */
+    @PostMapping("/login")
+    public String postLogin() {
+        return "redirect:/user/list";
     }
 }
