7b600d2fa0f77480066f2677d01d38a3f429c0c1
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-30 22:48:54 +0300

    add crud method getAll

--- src/main/java/com/example/spring_boot_project/repository/UserMapper.java
+++ src/main/java/com/example/spring_boot_project/repository/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.spring_boot_project.repository;
 import com.example.spring_boot_project.entity.UserMaster;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
 
@@ -11,4 +13,8 @@ public interface UserMapper {
      */
     public int insertOne(UserMaster userMaster);
 
+    /**
+     * Get user
+     */
+    public List<UserMaster> findMany();
 }
--- src/main/java/com/example/spring_boot_project/service/UserService.java
+++ src/main/java/com/example/spring_boot_project/service/UserService.java
@@ -2,11 +2,18 @@ package com.example.spring_boot_project.service;
 
 import com.example.spring_boot_project.entity.UserMaster;
 
+import java.util.List;
+
 public interface UserService {
 
     /**
      * User signup
      */
     public void signup(UserMaster userMaster);
+
+    /**
+     * Get all users
+     */
+    public List<UserMaster> getUsers();
 
 }
